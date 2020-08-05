093ed4fcbe599c5a8dd66a241924fc88ade9dda5
Vanessa Lane <vlane@23andme.com>
2020-08-04 16:04:42 -1000

    add get method for candidates

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const sqlite3 = require('sqlite3').verbose();
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -7,11 +8,58 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+// Create DB
+const db = new sqlite3.Database("./db/election.db", err => {
+    if (err) {
+        return console.error(err.message);
+    }
+
+    console.log('Connected to the election database.');
+})
+
+// get all candidates
+app.get('/api/candidates', (req, res) => {
+    const sql = `SELECT * FROM candidates`;
+    const params = [];
+    db.all(sql, params, (err, rows) => {
+        if (err) {
+            res.status(500).json({error: err.message});
+            return;
+        }
+
+        res.json({
+            message: 'success',
+            data: rows
+        });
+    });
+});
+
+// Get single candidate
+app.get('/api/candidate/:id', (req, res) => {
+    const sql = `SELECT * FROM candidates
+                 WHERE id = ?`;
+    const params = [req.params.id];
+    db.get(sql, params, (err, row) => {
+      if (err) {
+        res.status(400).json({ error: err.message });
+        return;
+      }
+
+      res.json({
+        message: 'success',
+        data: row
+      });
+    });
+  });
+
 // Default response for any other requests (Not Found)
 app.use((req, res) => {
   res.status(404).end();
 });
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+// start server after DB connection
+db.on('open', () => {
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+    });
+})
