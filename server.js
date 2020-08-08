9d7e61a99d27f2649b001c5a6e2177cb3230e4b0
Vanessa Lane <vlane@23andme.com>
2020-08-08 10:47:29 -1000

    add get/post/delete endpoints

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const sqlite3 = require('sqlite3').verbose();
+const inputCheck = require('./utils/inputCheck');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -40,26 +41,61 @@ app.get('/api/candidate/:id', (req, res) => {
                  WHERE id = ?`;
     const params = [req.params.id];
     db.get(sql, params, (err, row) => {
-      if (err) {
-        res.status(400).json({ error: err.message });
-        return;
-      }
+        if (err) {
+            res.status(400).json({ error: err.message });
+            return;
+        }
 
-      res.json({
-        message: 'success',
-        data: row
-      });
+        res.json({
+            message: 'success',
+            data: row
+        });
     });
-  });
-
-// Default response for any other requests (Not Found)
-app.use((req, res) => {
-  res.status(404).end();
 });
 
-// start server after DB connection
-db.on('open', () => {
-    app.listen(PORT, () => {
-      console.log(`Server running on port ${PORT}`);
+// Delete a candidate
+app.delete('/api/candidate/:id', (req, res) => {
+    const sql = `DELETE FROM candidates WHERE id = ?`;
+    const params = [req.params.id];
+    db.run(sql, params, function(err, result) {
+        if (err) {
+            res.status(400).json({ error: res.message });
+            return;
+        }
+    
+        res.json({
+            message: 'successfully deleted',
+            changes: this.changes
+        });
+    });
+});
+
+// Create a candidate
+app.post('/api/candidate', ({ body }, res) => {
+    const errors = inputCheck(body, 'first_name', 'last_name', 'industry_connected');
+    if (errors) {
+        res.status(400).json({ error: errors });
+        return;
+    }
+    
+    const sql = `INSERT INTO candidates (first_name, last_name, industry_connected) 
+              VALUES (?,?,?)`;
+    const params = [body.first_name, body.last_name, body.industry_connected];
+    // ES5 function, not arrow function, to use `this`
+    db.run(sql, params, function(err, result) {
+    if (err) {
+        res.status(400).json({ error: err.message });
+        return;
+    }
+
+    res.json({
+        message: 'success',
+        data: body,
+        id: this.lastID
+    });
     });
 })
+
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
+});
