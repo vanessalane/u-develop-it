b9b66f4f5f9de8a9c14065817350d91aa1472275
Vanessa Lane <vlane@23andme.com>
2020-08-08 13:38:45 -1000

    add voter tableschema and seeds

--- db/schema.sql
+++ db/schema.sql
@@ -1,5 +1,6 @@
 DROP TABLE IF EXISTS parties;
 DROP TABLE IF EXISTS candidates;
+DROP TABLE IF EXISTS voters;
 
 CREATE TABLE candidates (
   id INTEGER PRIMARY KEY,
@@ -15,3 +16,11 @@ CREATE TABLE parties (
   name VARCHAR(50) NOT NULL,
   description TEXT
 );
+
+CREATE TABLE voters (
+  id INTEGER PRIMARY KEY,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  email VARCHAR(50) NOT NULL,
+  created_at DATETIME DEFAULT CURRENT_TIMESTAMP
+)
